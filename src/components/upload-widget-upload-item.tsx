59047cb7ff7be4b1a34cb9fb76f41b45d259aa3c
Deibson Cogo <deibsoncogo@outlook.com>
2025-02-11 20:25:47 -0300

    fix: realizando alguns ajustes nos códigos

--- src/components/upload-widget-upload-item.tsx
+++ src/components/upload-widget-upload-item.tsx
@@ -2,6 +2,7 @@ import * as Progress from "@radix-ui/react-progress"
 import { Download, ImageUp, Link2, RefreshCcw, X } from "lucide-react"
 import { motion } from "motion/react"
 import { type Upload, useUploads } from "../store/uploads"
+import { downloadUrl } from "../utils/download-url"
 import { formatBytes } from "../utils/format-bytes"
 import { Button } from "./ui/button"
 
@@ -15,6 +16,7 @@ export function UploadWidgetUploadItem({
   uploadId,
 }: UploadWidgetUploadItemProps) {
   const cancelUpload = useUploads((store) => store.cancelUpload)
+  const retryUpload = useUploads((store) => store.retryUpload)
 
   const progress = Math.min(
     upload.compressedSizeInBytes
@@ -27,13 +29,15 @@ export function UploadWidgetUploadItem({
     <motion.div
       className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/2 relative overflow-hidden"
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{
+        opacity: 1,
+      }}
       transition={{ duration: 0.4 }}
     >
       <div className="flex flex-col gap-1">
         <span className="text-xs font-medium flex items-center gap-1">
           <ImageUp className="size-3 text-zinc-300" strokeWidth={1.5} />
-          <span>{upload.name}</span>
+          <span className="max-w-[180px] truncate">{upload.name}</span>
         </span>
 
         <span className="text-xxs text-zinc-400 flex gap-1.5 items-center">
@@ -77,11 +81,17 @@ export function UploadWidgetUploadItem({
       </Progress.Root>
 
       <div className="absolute top-2 right-2 flex items-center gap-1">
-        <Button size="icon-sm" aria-disabled={upload.status !== "success"} asChild>
-          <a href={upload.remoteUrl}>
-            <Download className="size-4" strokeWidth={1.5} />
-            <span className="sr-only">Download compressed image</span>
-          </a>
+        <Button
+          size="icon-sm"
+          aria-disabled={!upload.remoteUrl}
+          onClick={() => {
+            if (upload.remoteUrl) {
+              downloadUrl(upload.remoteUrl)
+            }
+          }}
+        >
+          <Download className="size-4" strokeWidth={1.5} />
+          <span className="sr-only">Download compressed image</span>
         </Button>
 
         <Button
@@ -95,7 +105,11 @@ export function UploadWidgetUploadItem({
           <span className="sr-only">Copy remote URL</span>
         </Button>
 
-        <Button disabled={!["canceled", "error"].includes(upload.status)} size="icon-sm">
+        <Button
+          disabled={!["canceled", "error"].includes(upload.status)}
+          size="icon-sm"
+          onClick={() => retryUpload(uploadId)}
+        >
           <RefreshCcw className="size-4" strokeWidth={1.5} />
           <span className="sr-only">Retry upload</span>
         </Button>
